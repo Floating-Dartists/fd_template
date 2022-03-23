eb098fbe545a179984cc47344083ac5d31487594
Guillaume Roux <rouxguillaume8@gmail.com>
2022-03-23 18:08:19 +0100

    added router

---
+++ lib/router.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:go_router/go_router.dart';
+
+import 'presentation/home_page/home_page.dart';
+
+GoRouter routerGenerator({String? initialLocation}) {
+  return GoRouter(
+    initialLocation: initialLocation ?? Routes.root,
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: Routes.root,
+        builder: (context, state) => const MyHomePage(),
+      ),
+    ],
+  );
+}
+
+class Routes extends Equatable {
+  static const root = '/';
+
+  @override
+  List<Object?> get props => [root];
+}
